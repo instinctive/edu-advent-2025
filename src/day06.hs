e96aa523902679984a41d62eb2234e1b0324d909
instinctive <177694+instinctive@users.noreply.github.com>
2025-12-06 05:14:27 -0800

    Day 06

--- src/day06.hs
+++ src/day06.hs
@@ -1,8 +1,20 @@
 #include "header.hs"
 
-main = getContents >>= print . solve . parse
+main = getContents >>= print . bimap solve solve . (parse1 &&& parse2)
 
-parse = transpose . map words . reverse . lines
+parse1 = transpose . map words . reverse . lines
+
+parse2 =
+    map concat .
+    splitOn [[]] .
+    map words .
+    transpose .
+    quux .
+    lines
+  where
+    -- move last line to first and add spaces after it
+    quux ll = ops : map (const ' ') ops : reverse args
+      where (ops:args) = reverse ll
 
 solve probs =
     sum $ map app probs
