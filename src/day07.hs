fad93290353ff68e9a355bc91cd024dd63f3828d
instinctive <177694+instinctive@users.noreply.github.com>
2025-12-08 08:30:48 -0800

    Day 07

---
+++ src/day07.hs
@@ -0,0 +1,40 @@
+#include "header.hs"
+
+main = getContents >>= print . (solve1 &&& solve2) . parse
+
+parse = lines
+
+solve1 (l:ll) =
+    flip execState 0 $
+    foldM go start ll
+  where
+    start = [ bool c '|' $ c == 'S' | c <- l ]
+    go (_:'|':_:aa) (_:'^':_:xx) =
+        modify' succ >>
+        ("|." <>) <$> go ('|':aa) ('?':xx)
+    go (a:aa) (_:xx) =
+        (a:) <$> go aa xx
+    go _ _ = pure []
+
+solve2 (l:ll) =
+    ans
+  where
+    Just (_,ans) = find ((=='S').fst) $ zip l $ cands
+    cands = foldr go (repeat 1) ll
+    go qq nn =
+        [ if b == '^' then x + z else y
+        | ((_,x):(b,y):(_,z):_) <- tails $ zip (wrap '?' qq) (wrap 1 nn) ]
+
+wrap q l = [q] <> l <> [q]
+
+-- solve2 ll =
+--     find ((=='S').fst) $
+--     zip (head ll) $
+--     foldr goLine (repeat 1) ll
+--   where
+--     goLine :: [Char] -> [Int] -> [Int]
+--     goLine qq nn =
+--         let nn' = [1] <> nn <> [1] in
+--         [ if hat b then x + z else y
+--         | ((a,x):(b,y):(c,z):_) <- tails (zip (wrap qq) nn')
+--         ]
